cdfd06012f4bafce9dfed109023b08bf34fa0e3f
Kauanw10 <kauanw10@gmail.com>
2026-03-14 12:19:53 -0300

    Atualizando env para deploy

--- config/env.php
+++ config/env.php
@@ -1,19 +1,20 @@
 <?php
-    define('DB_HOST', 'mysql-1303todolist-kauanw10-db1.f.aivencloud.com');
-    define('DB_NAME', 'defaultdb');
-    define('DB_USER', 'avnadmin');
-    define('DB_PASS', '');
-    define('DB_PORT', '10745');
-    define('ENV', 'prod');
+
+    define('DB_HOST', getenv('DB_HOST') ?: 'mysql-1303todolist-kauanw10-db1.f.aivencloud.com');
+    define('DB_NAME', getenv('DB_NAME') ?: 'defaultdb');
+    define('DB_USER', getenv('DB_USER') ?: 'avnadmin');
+    define('DB_PASS', getenv('DB_PASS') ?: '');
+    define('DB_PORT', getenv('DB_PORT') ?: '10745');
+    
+    define('ENV', getenv('RENDER') ? 'prod' : 'dev');
 
     if (ENV === 'dev') {
         ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
-        ini_set('log_errors', 0);
     } else {
         ini_set('display_errors', 0);
         ini_set('log_errors', 1);
-        ini_set('error_log', __DIR__ . '/../logs/erros.log');
         ini_set('display_startup_errors', 0);
     }
 ?>
