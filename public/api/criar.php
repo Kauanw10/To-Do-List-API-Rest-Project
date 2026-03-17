40b108aee502f6e80ddeaf6f8357db59d5ad9e94
Kauanw10 <kauanw10@gmail.com>
2026-03-17 00:22:32 -0300

    Adicionando Debags e corrigindo rotas.

--- public/api/criar.php
+++ public/api/criar.php
@@ -1,10 +1,12 @@
 <?php
 // Força o PHP a mostrar erros na tela para o JS capturar
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../../config/env.php';
 require_once __DIR__ . '/../../services/TarefaService.php';
+require_once __DIR__ . '/../../controllers/funcoes.php';
 
 // CAPTURA O JSON DO JAVASCRIPT
 $json = file_get_contents('php://input');
