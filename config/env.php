a60243685f8651bab4c71f7df00ce634279ad125
Kauanw10 <kauanw10@gmail.com>
2026-05-26 23:33:24 -0300

    Alteração de Dados do Banco

--- config/env.php
+++ config/env.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', getenv('DB_HOST') ?: 'mysql-1303todolist-kauanw10-db1.f.aivencloud.com');
-    define('DB_NAME', getenv('DB_NAME') ?: 'defaultdb');
+    define('DB_NAME', getenv('DB_NAME') ?: 'tarefas_list_api');
     define('DB_USER', getenv('DB_USER') ?: 'avnadmin');
     define('DB_PASS', getenv('DB_PASS') ?: '');
     define('DB_PORT', getenv('DB_PORT') ?: '10745');
